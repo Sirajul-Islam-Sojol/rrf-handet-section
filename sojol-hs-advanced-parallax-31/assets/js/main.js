19e3f2597765a6d7561f71ede830b5e0bec78456
Sirajul-Islam-Sojol <mdsirajul182@gmail.com>
2017-06-05 11:34:15 +0600

    advanced-parallax-31

---
+++ sojol-hs-advanced-parallax-31/assets/js/main.js
@@ -0,0 +1,22 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+        
+        var s = skrollr.init({
+            render: function(data){
+                //Debugging - Log the current scroll position.
+                //console.log(data.curTop);
+            }
+        });
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery)); 
