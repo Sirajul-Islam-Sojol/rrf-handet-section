b94da3a39fc9639646eaf786f3c0e4d34ffa7274
Sirajul-Islam-Sojol <mdsirajul182@gmail.com>
2017-06-19 17:19:17 +0600

    sojol-rrf-pad-to-html-8

---
+++ sojol-rrf-pad-to-html-8/assets/js/main.js
@@ -0,0 +1,49 @@
+(function ($) {
+  "use strict";
+
+    jQuery(document).ready(function($){
+
+
+       $(".case-studies-carousel").owlCarousel({
+            items: 3,
+            margin:30,
+            loop:true,
+            dots:true,
+            nav:false,
+            autoplay: true,
+       });
+        $(".logo-carousel").owlCarousel({
+            items: 6,
+            margin:30,
+            loop:true,
+            dots:false,
+            nav:false,
+            autoplay: true,
+       });
+
+       $(".testimonial-carousel").owlCarousel({
+            items: 1,
+            loop:true,
+            dots:true,
+            nav:false,
+            autoplay: true,
+            
+       });
+
+        $(".testimonial-item").owlCarousel({
+            items: 1,
+            loop:true,
+            dots:true,
+            nav:false,
+            autoplay: true,
+            
+       });
+
+    });
+
+    jQuery(window).load(function(){
+        
+    });
+
+
+}(jQuery)); 
